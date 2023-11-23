629577a1b4015afa8aece77498fe129ac54ed7d7
Steffany Ouverney <steffanyouverney@gmail.com>
2023-11-23 11:22:49 -0300

    Boas práticas

--- Ignitor/BasicCode.py
+++ Ignitor/BasicCode.py
@@ -1,42 +1,52 @@
 import RPi.GPIO as GPIO
 import time
 
+# Configuração dos pinos
 GPIO.setmode(GPIO.BCM) # BCM números depois do GPIO
 
 led = 24
 buzzer = 8
 relay = 16
 
+# Configuração dos pinos como saída
 GPIO.setup(led, GPIO.OUT)
 GPIO.setup(buzzer, GPIO.OUT)
 GPIO.setup(relay, GPIO.OUT)
 
+# Inicialização dos pinos
 GPIO.output(buzzer, GPIO.LOW)
 GPIO.output(led, GPIO.LOW)
 GPIO.output(relay, GPIO.HIGH)
 
-button_press = True # Estado do botão
+# Estado do botão
+button_press = True 
 
+# Se o botão estiver pressionado
 if button_press:
-    for contagem_regre in range(10, 0, -1):
-       GPIO.output(buzzer, GPIO.HIGH)
-       print(contagem_regre)
-       time.sleep(0.2)
-       GPIO.output(buzzer, GPIO.LOW)
-       print("Beep")
-       time.sleep(0.8)
+  # Contagem regressiva
+  for contagem_regre in range(10, 0, -1):
+      # Liga o buzzer
+      GPIO.output(buzzer, GPIO.HIGH)
+      print(contagem_regre)
+      time.sleep(0.2)
+      # Desliga o buzzer
+      GPIO.output(buzzer, GPIO.LOW)
+      print("Beep")
+      time.sleep(0.8)
 
-       #Opção para cancelar a ignição
-       if(button_press == False):
-           print("Botão cancelar pressionado, relé desligado.")
-           GPIO.output(relay, GPIO.HIGH)
-           GPIO.output(buzzer, GPIO.LOW)
-           break
+      # Opção para cancelar a ignição
+      if(button_press == False):
+          print("Botão cancelar pressionado, relé desligado.")
+          GPIO.output(relay, GPIO.HIGH)
+          GPIO.output(buzzer, GPIO.LOW)
+          break
 
-    GPIO.output(buzzer, GPIO.LOW)
-    GPIO.output(led, GPIO.HIGH)
-    GPIO.output(relay, GPIO.LOW)
-    print("\nRelé e led acionados.")
-    time.sleep(5)
+  # Liga o led e desliga o relé
+  GPIO.output(buzzer, GPIO.LOW)
+  GPIO.output(led, GPIO.HIGH)
+  GPIO.output(relay, GPIO.LOW)
+  print("\nRelé e led acionados.")
+  time.sleep(5)
 
-GPIO.cleanup() # Limpar/desconfigurar todos os pinos GPIO
+# Limpeza dos pinos
+GPIO.cleanup() 
